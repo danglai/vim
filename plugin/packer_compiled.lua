2fc3501bdba72829af74d4f38125fd7c536eacb8
lai <geotech@geotechs-MacBook-Pro.local>
2022-08-25 11:23:54 +0700

    fix

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -155,7 +155,7 @@ vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Filetype lazy-loads
 time([[Defining lazy-load filetype autocommands]], true)
-vim.cmd [[au FileType python ++once lua require("packer.load")({'vim-python-pep8-indent', 'semshi'}, { ft = "python" }, _G.packer_plugins)]]
+vim.cmd [[au FileType python ++once lua require("packer.load")({'semshi', 'vim-python-pep8-indent'}, { ft = "python" }, _G.packer_plugins)]]
 time([[Defining lazy-load filetype autocommands]], false)
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
